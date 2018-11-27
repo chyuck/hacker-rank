0d005a823eb541c2902a5797b78d1534fe20a2e3
Andrey Sergeev <andrey@chyuck.com>
2018-11-27 18:30:54 -0500

    Added Counting Valleys problem

---
+++ CountingValleys.cs
@@ -0,0 +1,56 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, string s) {
+        var result = 0;
+        var altitude = 0;
+        
+        foreach (var item in s)
+        {
+            switch (item)
+            {
+                case 'U':
+                    altitude++;
+                    break;
+                    
+                case 'D':
+                    altitude--;
+                    if (altitude == -1) result++;
+                    break;
+                    
+                default: throw new InvalidOperationException();
+            }
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        int result = countingValleys(n, s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 | Problem | Solution |
 | --- | --- |
 | [Sock Merchant](https://www.hackerrank.com/challenges/sock-merchant) | [SockMerchant.cs](SockMerchant.cs) |
+| [Counting Valleys](https://www.hackerrank.com/challenges/counting-valleys) | [CountingValleys.cs](CountingValleys.cs) |
