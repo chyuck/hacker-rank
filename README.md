7ce7bdc4c1f082dd33d3bfc4d3317f5ab432d505
Andrey Sergeev <andrey@chyuck.com>
2018-11-27 18:14:19 -0500

    Added Sock Merchant

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [hackerrank.com](https://hackerrank.com) problem solutions using C#
+
+| Problem | Solution |
+| --- | --- |
+| [Sock Merchant](https://www.hackerrank.com/challenges/sock-merchant) | [SockMerchant.cs](SockMerchant.cs) |
---
+++ SockMerchant.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        if (ar.Length != n) throw new ArgumentException("ar.Length != n");
+
+        var result = 0;
+        var unpairedSocks = new HashSet<int>();
+        
+        foreach (var item in ar) 
+        {
+            if (!unpairedSocks.Contains(item)) 
+            {
+                unpairedSocks.Add(item);
+                continue;
+            }
+
+            result++;
+            unpairedSocks.Remove(item);
+        }
+
+        return result;
+    }    
+    
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+        int result = sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
